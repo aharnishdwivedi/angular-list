4efbadbf0af00b445cc85cf787a6dd70ea24b7fc
Aharnish Dwivedi <55889077+aharnishdwivedi@users.noreply.github.com>
2023-06-06 00:38:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: "./app.component.html",
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-api-example';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  apiList!: any[]; 
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getList().subscribe(data => {
+      this.apiList = data.entries;
+    });
+  }
 }
