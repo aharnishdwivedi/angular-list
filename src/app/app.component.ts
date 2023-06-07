5cafebabfaaaa511df3f19603291385e8758ba37
Aharnish Dwivedi <55889077+aharnishdwivedi@users.noreply.github.com>
2023-06-07 13:58:35 +0530

    try2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,37 @@ import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: "./app.component.html",
+  template: `
+    <h1>API List</h1>
+    <input type="text" [(ngModel)]="searchTerm" placeholder="Search API" />
+    <button (click)="search()">Search</button>
+    <ul>
+      <li *ngFor="let entry of filteredApiList">{{ entry.API }}</li>
+    </ul>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title(title: any) {
-    throw new Error('Method not implemented.');
-  }
-  apiList!: any[]; 
+  apiList!: any[];
+  filteredApiList!: any[];
+  searchTerm: string = '';
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
     this.apiService.getList().subscribe(data => {
       this.apiList = data.entries;
+      this.filteredApiList = this.apiList;
     });
+  }
+
+  search() {
+    if (this.searchTerm.trim() === '') {
+      this.filteredApiList = this.apiList;
+    } else {
+      this.filteredApiList = this.apiList.filter((entry: any) =>
+        entry.API.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
   }
 }
